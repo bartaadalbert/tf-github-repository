01885d8f5c38cdce86b9b8ce74dd8ba735254d0f
Adalbert Barta <adalbertbarta@outbix.com>
2023-06-14 14:15:15 +0200

    git existing

--- main.tf
+++ main.tf
@@ -3,7 +3,12 @@ provider "github" {
   token = var.github_token
 }
 
+data "github_repository" "existing" {
+  full_name = "${var.github_owner}/${var.repository_name}"
+}
+
 resource "github_repository" "this" {
+  count      = data.github_repository.existing.id != null ? 0 : 1
   name       = var.repository_name
   visibility = var.repository_visibility
   auto_init  = true
