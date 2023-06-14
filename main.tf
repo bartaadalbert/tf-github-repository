62a6065d0c54932ac4464c7c939d1e3d4d320532
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-14 17:24:03 +0200

    prevent destroy

--- main.tf
+++ main.tf
@@ -12,6 +12,9 @@ resource "github_repository" "this" {
   name       = var.repository_name
   visibility = var.repository_visibility
   auto_init  = true
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "github_repository_deploy_key" "this" {
