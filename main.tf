5e2f4a56cbed0bcb909a71ef75fe702cb2732a99
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-17 00:27:52 +0200

    handle the github repos

--- main.tf
+++ main.tf
@@ -7,18 +7,19 @@ data "github_repository" "existing" {
   full_name = "${var.github_owner}/${var.repository_name}"
 }
 
+locals {
+  repository_exists = try(data.github_repository.existing.id, null) != null ? 0 : 1
+}
+
 resource "github_repository" "this" {
-  count      = data.github_repository.existing.id != null ? 0 : 1
+  count      = local.repository_exists
   name       = var.repository_name
   visibility = var.repository_visibility
   auto_init  = true
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
 }
 
 resource "github_repository_deploy_key" "this" {
-  count      = data.github_repository.existing.id != null ? 0 : 1
+  count      = local.repository_exists
   title      = var.public_key_openssh_title
   repository = github_repository.this[count.index].name
   key        = var.public_key_openssh
