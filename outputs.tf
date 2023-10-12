1b24c1fcf1a9e6c2a073cc003414b0db15ccc625
Adalbert Barta <adalbertbarta@gmail.com>
2023-10-12 23:40:51 +0200

    repo index

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,6 @@
 output "repository_name" {
   description = "The name of the created repository."
-  value       = try(github_repository.this.name, null)
+  # value       = try(github_repository.this.name, null)
+  value = try(github_repository.this[0].name, null)
 }
 
